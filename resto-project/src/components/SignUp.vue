dba8fd3a8c9e5c57902d1ac42d42d244dac0246c
satyasowmya <satyasowmya0@gmail.com>
2022-12-26 16:36:30 +0530

    restaurant vuejs project

---
+++ resto-project/src/components/SignUp.vue
@@ -0,0 +1,64 @@
+<template>
+<img class="logo" alt="Vue logo" src="../assets/resto.jpg">
+<h1>Sign Up</h1>
+<div class="register">
+    <input type="text" v-model="name" placeholder="Enter Name" />
+    <input type="text" v-model="email" placeholder="Enter Email" />
+    <input type="password" v-model="password" placeholder="Enter Password" />
+    <button v-on:click="signUp">Sign Up</button>
+</div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+    name: "Sign Up",
+    data() {
+        return {
+            name: '',
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        async signUp() {
+            let result = await axios.post("http://localhost:3000/user",{
+                name:this.name,
+                email:this.email,
+                password:this.password
+            });
+            console.log(result)
+            if(result.status==201){
+                alert("sign-up done")
+                localStorage.setItem("user-info",JSON.stringify(result.data))
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.logo {
+    width: 200px;
+}
+
+.register input {
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid skyblue;
+}
+
+.register button {
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: #fff;
+    cursor: pointer;
+}
+</style>
